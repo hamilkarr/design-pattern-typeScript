5d6669adbba64213241982935df0e68569bd95d5
hamilkarr <12uclock@gmail.com>
2024-06-14 06:19:48 +0900

    LocalStorage 구현 연습

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,88 @@
-interface SStorage<T> {
+interface CustomStorage<T> {
     [key: string]: T;
 }
 
-abstract class LocalStorage<T> {
-    private storage: SStorage<T> = {};
-    setItem(key: string, value: T) {}
-    getItem(key: string) {}
-    clearItem(key: string) {}
-    clear() {}
+abstract class AbstractLocalStorage<T> {
+    protected storage: CustomStorage<T> = {};
+    abstract setItem(key: string, value: T): void;
+    abstract getItem(key: string): T | null;
+    abstract clearItem(key: string): void;
+    abstract clear(): void;
+}
+
+class LocalStorage<T> extends AbstractLocalStorage<T> {
+    setItem(key: string, value: T) {
+        this.storage[key] = value;
+    }
+
+    getItem(key: string): T | null {
+        return this.storage[key] || null;
+    }
+
+    clearItem(key: string) {
+        delete this.storage[key];
+    }
+
+    clear() {
+        this.storage = {};
+    }
+}
+
+type SuccessCallback = (latitude: number, longitude: number) => void;
+type target = {
+    latitude: number;
+    longitude: number;
+};
+
+type GetCurrentPosition = {
+    (successFn: SuccessCallback): void;
+    (successFn: SuccessCallback, errorFn: ErrorCallback): void;
+    (
+        successFn: SuccessCallback,
+        errorFn: ErrorCallback,
+        optionsObj: PositionOptions
+    ): void;
+};
+type watchPosition = {
+    (successFn: SuccessCallback): void;
+    (successFn: SuccessCallback, errorFn: ErrorCallback): void;
+    (
+        successFn: SuccessCallback,
+        errorFn: ErrorCallback,
+        optionsObj: PositionOptions
+    ): void;
+};
+
+class GeolocationAPI {
+    private target: target = {
+        latitude: 0,
+        longitude: 0,
+    };
+    getCurrentPosition: GetCurrentPosition = (
+        successFn: SuccessCallback,
+        errorFn?: ErrorCallback,
+        optionsObj?: PositionOptions
+    ) => {
+        if (errorFn && optionsObj) {
+            this.getCurrentPosition(successFn, errorFn, optionsObj);
+        } else if (errorFn) {
+            this.getCurrentPosition(successFn, errorFn);
+        } else {
+            this.getCurrentPosition(successFn);
+        }
+    };
+
+    watchPosition: watchPosition = (
+        successFn: SuccessCallback,
+        errorFn?: ErrorCallback,
+        optionsObj?: PositionOptions
+    ) => {
+        if (errorFn && optionsObj) {
+            this.watchPosition(successFn, errorFn, optionsObj);
+        } else if (errorFn) {
+            this.watchPosition(successFn, errorFn);
+        } else {
+            this.watchPosition(successFn);
+        }
+    };
 }
